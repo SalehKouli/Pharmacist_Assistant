02fc5ae1bae73953b4e76075c88bd730c6750284
SalehKouli <saleh.kouli28@gmail.com>
2024-09-08 02:30:32 +0300

    1

--- app/src/main/java/com/example/pharmacistassistant/Constants.kt
+++ app/src/main/java/com/example/pharmacistassistant/Constants.kt
@@ -2,6 +2,6 @@ package com.example.pharmacistassistant
 
 // Constants to represent the versionCode and versionName
 object AppConstants {
-    const val VERSION_CODE = 2 // Replace with your versionCode from build.gradle.kts
+    const val VERSION_CODE = 1 // Replace with your versionCode from build.gradle.kts
     const val VERSION_NAME = "1.0" // Replace with your versionName from build.gradle.kts
 }
